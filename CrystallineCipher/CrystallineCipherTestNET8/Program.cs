27a4bb87a5ad2d227aa09fd1cd5147aa69bb2dd5
Mark McCarron <info@marxbitware.co.uk>
2023-12-19 20:37:52 +0000

    Added Crystalline 4

--- CrystallineCipher/CrystallineCipherTestNET8/Program.cs
+++ CrystallineCipher/CrystallineCipherTestNET8/Program.cs
@@ -9,12 +9,12 @@ namespace CrystallineCipherTestNET8
             int rounds = 16;
 
             //Crystalline 3
-            Console.WriteLine("Crystalline 3");
-            Console.WriteLine("Encrypting plain text...");
-            File.WriteAllBytes(@"..\..\..\TestFiles\ciphertext3.txt", Crystalline3.Encrypt(File.ReadAllBytes(@"..\..\..\TestFiles\plaintext.txt"), File.ReadAllBytes(@"..\..\..\TestFiles\k.rng"), File.ReadAllBytes(@"..\..\..\TestFiles\s.rng"), File.ReadAllBytes(@"..\..\..\TestFiles\s2.rng"), rounds));
+            Console.WriteLine("Crystalline 4");
+            Console.WriteLine("Encrypting...");
+            File.WriteAllBytes(@"..\..\..\TestFiles\ciphertext4.txt", Crystalline4.Encrypt(File.ReadAllBytes(@"..\..\..\TestFiles\plaintext.txt"), File.ReadAllBytes(@"..\..\..\TestFiles\k.rng"), File.ReadAllBytes(@"..\..\..\TestFiles\s.rng"), File.ReadAllBytes(@"..\..\..\TestFiles\s2.rng"), rounds));
 
-            Console.WriteLine("Decrypting plain text...");
-            File.WriteAllBytes(@"..\..\..\TestFiles\decipheredplaintext3.txt", Crystalline3.Decrypt(File.ReadAllBytes(@"..\..\..\TestFiles\ciphertext3.txt"), File.ReadAllBytes(@"..\..\..\TestFiles\k.rng"), File.ReadAllBytes(@"..\..\..\TestFiles\s.rng"), File.ReadAllBytes(@"..\..\..\TestFiles\s2.rng"), rounds));
+            Console.WriteLine("Decrypting...");
+            File.WriteAllBytes(@"..\..\..\TestFiles\decipheredplaintext4.txt", Crystalline4.Decrypt(File.ReadAllBytes(@"..\..\..\TestFiles\ciphertext4.txt"), File.ReadAllBytes(@"..\..\..\TestFiles\k.rng"), File.ReadAllBytes(@"..\..\..\TestFiles\s.rng"), File.ReadAllBytes(@"..\..\..\TestFiles\s2.rng"), rounds));
         }
     }
 }
